401a90dd43b83a9a86e78d9a9f6cc46383002b1b
Amine <vipamine@gmailcom>
2018-04-01 12:08:45 +0200

    UPDATE

--- app/src/main/java/com/example/asus/startup/LoginActivity.java
+++ app/src/main/java/com/example/asus/startup/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.asus.startup;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -15,6 +21,9 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.List;
@@ -27,11 +36,44 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
         FirebaseApp.initializeApp(this);
 
         mAuth = FirebaseAuth.getInstance();
         // Choose authentication providers
+Intent i = new Intent(this,MainActivity.class);
 
+
+        if(isNetworkAvailable()) {
+
+    /* DO WHATEVER YOU WANT IF INTERNET IS AVAILABLE */
+
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(false);
+            builder.setTitle("No Internet");
+            builder.setMessage("Internet is required. Please Retry.");
+
+            builder.setNegativeButton("Stay Offline", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    LoginActivity.this.startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }
+            });
+
+            builder.setPositiveButton("Retry", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    dialog.dismiss();
+                    LoginActivity.this.recreate();
+                }
+            });
+            AlertDialog dialog = builder.create(); // calling builder.create after adding buttons
+            dialog.show();
+            Toast.makeText(this, "Network Unavailable!", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -91,6 +133,33 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+
+
+    public boolean hasActiveInternetConnection(Context context) {
+        if (isNetworkAvailable()) {
+            try {
+                HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+                urlc.setRequestProperty("User-Agent", "Test");
+                urlc.setRequestProperty("Connection", "close");
+                urlc.setConnectTimeout(1500);
+                urlc.connect();
+                return (urlc.getResponseCode() == 200);
+            } catch (IOException e) {
+                Log.e("Connection", "Error checking internet connection", e);
+            }
+        } else {
+            Log.d("Connection", "No network available!");
+        }
+        return false;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
     }
 }
 
